d29507af4fb083512b6e5ba2ba78060bb4623ab1
Amy Gill <amy.gill.34@gmail.com>
2018-04-12 16:11:00 -0400

    one method

---
+++ src/main/java/io/zipcoder/controller/AccountController.java
@@ -0,0 +1,20 @@
+package io.zipcoder.controller;
+
+import io.zipcoder.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AccountController {
+
+    @Autowired
+    AccountService accountService;
+
+    @RequestMapping(value = "/accounts", method = RequestMethod.GET)
+    public ResponseEntity getAllAccounts(){
+        return accountService.getAllAccounts();
+    }
+}
--- src/main/java/io/zipcoder/services/AccountService.java
+++ src/main/java/io/zipcoder/services/AccountService.java
@@ -1,5 +1,20 @@
 package io.zipcoder.services;
 
+import io.zipcoder.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public ResponseEntity getAllAccounts(){
+        Iterable accounts = accountRepository.findAll();
+        return new ResponseEntity(accounts, HttpStatus.OK);
+    }
 
 }
